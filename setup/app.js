fe816538a8d093854f51b2dd274a5167c072b4c1
aman-akhtar <amanakhtar881@gmail.com>
2023-08-21 23:04:32 +0530

    send color

--- setup/app.js
+++ setup/app.js
@@ -11,11 +11,18 @@ console.log(btns);
   item.addEventListener("click", function (e) {
     console.log(e.currentTarget.classList);
     let styles = e.currentTarget.classList;
+
     styles.contains("increase")
       ? (count = count + 1)
       : styles.contains("decrease")
       ? (count = count - 1)
       : (count = 0);
+    count > 0
+      ? (value.style.color = "red")
+      : count < 0
+      ? (value.style.color = "green")
+      : (value.style.color = "yellow");
     value.textContent = count;
   });
+  console.log(count);
 });
