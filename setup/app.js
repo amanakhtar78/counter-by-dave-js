0ff9d2eb3d067b65dd5c10dc0376c5bdcf2bfe1d
aman-akhtar <amanakhtar881@gmail.com>
2023-08-21 22:08:00 +0530

    1st commit counter

---
+++ setup/app.js
@@ -0,0 +1,21 @@
+let count = 0;
+const value = document.getElementById("value");
+const btns = document.getElementsByClassName("btn");
+
+console.log(btns);
+
+// Array.from(btns).forEach(function (item) {
+[...btns].forEach(function (item) {
+  console.log(item);
+
+  item.addEventListener("click", function (e) {
+    console.log(e.currentTarget.classList);
+    let styles = e.currentTarget.classList;
+    styles.contains("increase")
+      ? (count = count + 1)
+      : styles.contains("decrease")
+      ? (count = count - 1)
+      : (count = 0);
+    value.textContent = count;
+  });
+});
